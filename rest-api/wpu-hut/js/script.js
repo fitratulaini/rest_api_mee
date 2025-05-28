037371954bb9557999c83f84dfdf6791c204f41f
Fitratul Aini <fitratulllni@gmail.com>
2025-05-28 23:48:59 +0700

    perbaiki penanganan kategori pada navbar

--- rest-api/wpu-hut/js/script.js
+++ rest-api/wpu-hut/js/script.js
@@ -4,8 +4,8 @@ function tampilkanSemuaMenu() {
         let menu = data.menu;
         let content = '';
         $.each(menu, function(i,data) {
-           content += 
-           '<div class="col-md-4"><div class="card mb-3"><img src="img/menu/'+ data.gambar +'"class="card-img-top"><div class="card-body"><h5 class="card-title">'+ data.nama +'</h5><p class="card-text">'+ data.deskripsi +'</p><h5 class="card-title">RP.'+ data.harga +'</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>';
+            content += 
+            '<div class="col-md-4"><div class="card mb-3"><img src="img/menu/'+ data.gambar +'"class="card-img-top"><div class="card-body"><h5 class="card-title">'+ data.nama +'</h5><p class="card-text">'+ data.deskripsi +'</p><h5 class="card-title">RP.'+ data.harga +'</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>';
         });
         $('#daftar-menu').html(content);
     });
